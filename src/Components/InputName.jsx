98141492e3e76bc155e8295168599726f8971f89
Foysal Ahammed <43314398+FAsami@users.noreply.github.com>
2020-08-24 02:19:35 +0600

    Add subtitle

--- src/Components/InputName.jsx
+++ src/Components/InputName.jsx
@@ -4,6 +4,9 @@ function InputName({ handleInput, handleSubmit, inputValue }) {
   return (
     <div className="card card-body mt-4">
       <h3 className="text-center card-title">Enter your name </h3>
+      <p className="text-center">
+        (Get the Country name starts with your name's letter)
+      </p>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <input
--- src/Components/Letter.jsx
+++ src/Components/Letter.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Letter({ letter, country }) {
   return (
-    <div className="d-flex">
-      <h5 className="text-center bg-light mr-3">{letter} - </h5>
+    <div className="d-flex align-items-center">
+      <h5 className="text-center bg-light mr-3">{" " + letter} -</h5>
       <h5 className="text-center bg-light "> {country || "(not found)"}</h5>
     </div>
   );
